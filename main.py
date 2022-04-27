a24da52311cddbb7dbb202f8ac6abd43af447895
FrankieeH <fra.eherrera@gmail.com>
2022-04-26 18:17:50 -0700

    Added user input functions/ other functions, cleaning up code in main

--- main.py
+++ main.py
@@ -3,7 +3,58 @@ import parkingLotClass
 import random
 import string
 
+lot_squareft = 0
+parkingspot_squareft = 0
+num_of_cars = 0
 carArray = []
+
+
+
+
+def user_input_lot():
+
+	lot_squareft = input("Please enter the desired square footage for your parking lot or enter 0 for default: (Default is 2000 sq ft)")
+
+	if lot_squareft == '0':
+		lot_squareft = 2000
+
+	
+	return int(lot_squareft)
+
+def user_input_spot():
+
+	parkingspot_squareft = input("Please entered the desired square footage for each parking spot or enter 0 for default: (Default is 8x12 = 96 sqft)")
+
+	if parkingspot_squareft == '0':
+		parkingspot_squareft = 96
+
+	
+	return int(parkingspot_squareft)
+
+
+
+
+
+
+def user_input_cars():
+
+	cars_to_park = input('Please enter amount of cars you wish to attempt to park: ')
+
+	return int(cars_to_park)
+
+def create_cars(carAmount):
+
+	for num in range(0,carAmount):
+		letters_and_digits = string.ascii_letters + string.digits
+		result_str = ''.join((random.choice(letters_and_digits) for i in range(7)))
+
+		print('Car License Plate # for this car is: ',result_str)
+
+	#create a Car for each License Plate created and add them to Car Array
+		carArray.append(carClass.Car(result_str))
+		print('\n',carArray[num].license_plate_num)
+
+
 
 
 
@@ -12,26 +63,28 @@ print("Welcome to Frankie's Parking Lot Services\n")
 
 print("First let's create the Parking Lot")
 
+lot_squareft = user_input_lot()
+parkingspot_squareft = user_input_spot()
+
+
 #default parking lot square footage will be 2000 for now
 #default parking spot square footage will be 96 for now
 
 #we created a parking lot object from parkingLot Class
-lot1 = parkingLotClass.parkingLot(2000)
+lot1 = parkingLotClass.parkingLot(lot_squareft, parkingspot_squareft)
 
 print('\n',lot1,'\n')
+
+
+
+
 
 #next we need to create an array of cars with random License Plate #'s
 #we can play around with the total number of cars created to be less, equal to, or more than the parking lot can fit
 #default amount of cars/License plate numbers created FOR NOW is 20
 
-for num in range(0,20):
-	letters_and_digits = string.ascii_letters + string.digits
-	result_str = ''.join((random.choice(letters_and_digits) for i in range(7)))
+num_of_cars = user_input_cars()
 
-	print('Car License Plate # for this car is: ',result_str)
-
-	#create a Car for each License Plate created and add them to Car Array
-	carArray.append(carClass.Car(result_str))
-	print('\n',carArray[num].license_plate_num)
+create_cars(num_of_cars)
 
 
--- parkingLotClass.py
+++ parkingLotClass.py
@@ -4,7 +4,7 @@
 
 class parkingLot:
 
-	def __init__(self, square_footage, parking_spot_size =96):
+	def __init__(self, square_footage, parking_spot_size):
 		self.square_footage = square_footage
 		self.parking_spot_size = parking_spot_size
 		self.total_parking_spots = int(self.square_footage / self.parking_spot_size)
